6bc36328b6fd42dd6da424b7e83fb750f6486b7a
Alex Cherniuk <ts33kr@gmail.com>
2015-12-08 22:45:01 +0200

    fix a mistake

--- aux.go
+++ aux.go
@@ -58,7 +58,7 @@ func (aux *Aux) Apply(context *Context) error {
 // Depending on the implementation of an op, middleware can either
 // be stored separately in its structure, or be combined with the
 // service middleware, depending on the op settings & coding.
-func (aux *Aux) Intermediate(*Service) []Middleware { return ep.Middleware }
+func (aux *Aux) Intermediate(*Service) []Middleware { return aux.Middleware }
 
 // Implementation of the Operation interface; report the error that
 // might have occured during execution of the buiness logic implemented
